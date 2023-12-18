dbe09220b7c8730c587fe8c3673b7940f8d6087c
Zakaria Gtaib <zgtaib@e2r9p11.1337.ma>
2023-12-18 20:07:35 +0100

    so close

--- get_next_line.c
+++ get_next_line.c
@@ -6,67 +6,107 @@
 /*   By: zgtaib <zgtaib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 17:54:22 by zgtaib            #+#    #+#             */
-/*   Updated: 2023/12/17 20:54:07 by zgtaib           ###   ########.fr       */
+/*   Updated: 2023/12/18 18:59:40 by zgtaib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 
-// static char *joined(char *buffer, char *temp)
-// {
-// 	char *tp;
+static char *joined(char *buffer, char *temp)
+{
+	char *tp;
 	
-// 	tp = strjoin(buffer, temp);
-// 	free(buffer);
-// 	return (tp);
-// }
+	tp = strjoin(buffer, temp);
+	free(buffer);
+	return (tp);
+}
 static char	*ft_readit(int fd, char *buffer)
 {
 	char *temp;
 	int bytesread = 0;
-
+		if (!buffer)
+			buffer = ft_strdup("");
 	temp = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!temp)
+	{	
+		free(temp);
 		return(NULL);
+	}
 	while ((bytesread = read(fd, temp, BUFFER_SIZE)) > 0)
-	{
+	{		
 		if(bytesread < 0)
 		{
 			free(temp);
 			return (NULL);
 		}
 		temp[bytesread] = '\0';
-		 buffer = strjoin(buffer, temp);
-		 free(buffer);
+		 buffer = joined(buffer, temp);
 		if (ft_strchr(buffer, '\n') != 0)
 			break ;
-	}
+	}                       
+	// printf(" \n||%s||\n ", buffer);
+	free(temp);
 	return (buffer);	
 }
+static char *next(char *buffer)
+{
+	int x;
+	char	*remains;
+	int y;
 
+	x = 0;
+	y = 0;
+	while (buffer[x] != '\0' && buffer[x] != '\n')
+		x++;
+	if(!buffer[x])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	remains = (char *)malloc((ft_strlen(buffer) - x + 1) * sizeof(char));
+	if (!remains)
+	{	
+		free(buffer);
+		return (NULL);
+	}
+	x++;
+	while (buffer[x])
+	{
+		remains[y] = buffer[x];
+		x++;
+		y++;
+	}
+	remains[y] = '\0';
+	// printf("\n<<%s>>\n", remains);
+	free(buffer);
+	return (remains);
+}
 static char *lines(char *buffer)
 {
 	char *line;
 	int x;
+	int n;
 	
 	x = 0;
-	if (!buffer)
+	if (buffer[0] == '\0' || !buffer)
 		return (NULL);
 	while(buffer[x] != '\0' && buffer[x] != '\n')
 		x++;
-	line = (char *)malloc((x + 2) * sizeof(char));
+	n = x + 2;
+	printf("\n----%d----\n", n);
+	line = (char *)malloc(n * sizeof(char));
 	if(!line)
 		return (NULL);
 	x = 0;
-	while (x >= 0)
+	while (buffer[x] != '\0' && buffer[x] != '\n')
 	{
 		line[x] = buffer[x];
-		x++;
+		x++;	
 	}
-	buffer[x] ='\n';
+	line[x] = '\n';
 	x++;
-	buffer[x] = '\0'; 
+	line[x] = '\0'; 
 	return(line);
 }
 char *get_next_line(int fd)
@@ -76,17 +116,35 @@ char *get_next_line(int fd)
 	
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);	
-	buffer = ft_strdup("");
 	buffer = ft_readit(fd, buffer);
+	if (!buffer)
+		return (NULL);
 	line = lines(buffer);
-	
+	buffer = next(buffer);
+	// printf("<%s>\n", buffer);
 	return (line);
 }
+// void f()
+// {
+// 	system("leaks a.out");
+// }
+
+
 int main()
 {
+	// atexit(f);
 	int fd = open("line.txt", O_RDONLY);	
-
+	printf("<<<%s>>>", get_next_line(fd));
+	printf("\n-------------------------------------------\n");
 	printf("<<<%s>>>", get_next_line(fd));	
+	printf("\n-------------------------------------------\n");
+	printf("<<<%s>>>", get_next_line(fd));
+// 	printf("\n-------------------------------------------\n");
+// // 	// printf("<<<%s>>>", get_next_line(fd));
+// // 	// printf("\n-------------------------------------------\n");
+// // 	// printf("<<<%s>>>", get_next_line(fd));
+// // 	// printf("\n-------------------------------------------\n");
+// // 	// printf("<<<%s>>>", get_next_line(fd));
 	close(fd);
 }
 	
